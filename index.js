2d3195af70ea8a7bd54091bdc8cb94b9fafeb4da
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2023-08-10 22:56:31 -0300

    minute 17:17 slides

--- index.js
+++ index.js
@@ -2,3 +2,30 @@
 
 let scene = document.getElementById("scene")
 let parallacInstance = new Parallax(scene)
+
+//SWIPER
+
+let keys = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+]
+
+let slider = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 150,
+  centeredSlides: true,
+  mousewheel: true,
+  pagination: {
+    el: ".planet-links",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<div class="' + className + '">' + keys[index] + "</div>"
+    },
+  },
+})
