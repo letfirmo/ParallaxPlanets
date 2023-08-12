b709755e3b470b91e5910067777e58c8f19c0f3b
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2023-08-11 21:34:12 -0300

    attempt 1

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-//PARALLAX
+// LANDING PAGE ANIMATION
+var scene = document.getElementById("scene")
+var parallaxInstance = new Parallax(scene)
 
-let scene = document.getElementById("scene")
-let parallacInstance = new Parallax(scene)
-
-//SWIPER
-
-let keys = [
+var keys = [
   "Mercury",
   "Venus",
   "Earth",
@@ -15,8 +12,8 @@ let keys = [
   "Uranus",
   "Neptune",
 ]
-
-let slider = new Swiper(".swiper-container", {
+var slider = new Swiper(".swiper-container", {
+  // Optional parameters
   slidesPerView: "auto",
   spaceBetween: 150,
   centeredSlides: true,
@@ -30,9 +27,8 @@ let slider = new Swiper(".swiper-container", {
   },
 })
 
-// SLIDE ANIMATIONS
-
 slider.on("slideChange", function () {
+  console.log("SLIDE CHANGED")
   gsap.to(".slide-text span", 0.2, {
     x: "-100px",
   })
@@ -42,7 +38,7 @@ slider.on("slideChange", function () {
   gsap.to(".slide-detail span", 0.5, {
     x: "-1000px",
   })
-  gsap.to(".slide-detail-facts span", 0.5, {
+  gsap.to(".slide-detail-facts div", 0.5, {
     x: "-1000px",
   })
   gsap.to(".swiper-slide-active", 0.5, {
@@ -54,31 +50,84 @@ slider.on("slideChange", function () {
 })
 
 slider.on("slideChangeTransitionEnd", function () {
+  gsap.to(".swiper-slide .slide-img", 1, {
+    rotation: 10,
+  })
   gsap.to(".slide-text span", 0.2, {
     x: 0,
     delay: 0.1,
   })
-  gsap.to(".slide-text span", 0.2, {
+  gsap.to(".slide-text span", 0, {
     x: "100px",
   })
+
   gsap.to(".slide-number span", 0.2, {
     x: 0,
-    delay: 0.1,
   })
-  gsap.to(".slide-number span", 0.2, {
+  gsap.to(".slide-number span", 0, {
     x: "100px",
   })
-  gsap.to(".slide-detail span", 0.5, {
+
+  gsap.to(".slide-detail span", 0.2, {
     x: 0,
   })
-  gsap.to(".slide-detail-facts span", 0.5, {
+
+  gsap.to(".slide-detail-facts div", 0.5, {
     x: 0,
   })
+
   gsap.to(".swiper-slide-active", 0.5, {
     scale: 1,
     ease: Power4.easeOut,
   })
-  gsap.to(".swiper-slide .slide-img", 1, {
-    rotation: 10,
+
+  gsap.to(".swiper-slide-active .slide-text", 0, {
+    autoAlpha: 1,
   })
+  gsap.to(".swiper-slide-active .slide-number", 0, {
+    autoAlpha: 1,
+  })
+
+  gsap.to(".swiper-slide-next .slide-text", 0, {
+    autoAlpha: 0,
+  })
+  gsap.to(".swiper-slide-prev .slide-text", 0, {
+    autoAlpha: 0,
+  })
+
+  gsap.to(".swiper-slide-next .slide-number", 0, {
+    autoAlpha: 0,
+  })
+  gsap.to(".swiper-slide-prev .slide-number", 0, {
+    autoAlpha: 0,
+  })
+})
+
+gsap.to(".rockbg1", 2, {
+  y: 12,
+  repeat: -1,
+  yoyo: true,
+  delay: 0,
+})
+
+gsap.to(".swiper-slide-next .slide-text", 0, {
+  autoAlpha: 0,
+})
+gsap.to(".swiper-slide-prev .slide-text", 0, {
+  autoAlpha: 0,
+})
+
+gsap.to(".swiper-slide-next .slide-number", 0, {
+  autoAlpha: 0,
+})
+gsap.to(".swiper-slide-prev .slide-number", 0, {
+  autoAlpha: 0,
+})
+
+gsap.to(".swiper-slide", 0, {
+  scale: 0.85,
+})
+
+gsap.to(".swiper-slide-active", 0, {
+  scale: 1,
 })
